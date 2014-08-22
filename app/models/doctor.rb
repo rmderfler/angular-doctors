4799768df6f4e5effe39d4dc993ddb0c1d6a7b34
Derfler <rmderfler@gmail.com>
2014-08-22 13:04:29 -0700

    add uniqueness validation to doctor name

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,13 @@
 class Doctor < ActiveRecord::Base
   validates :name, :presence => true
+  validates :name, :uniqueness => true
+
+  before_save :capital_case_name
+
+    private
+
+  def capital_case_name
+    self.name = self.name.capitalize!
+  end
+  
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe Doctor do
   it { should validate_presence_of :name }
 
+   it { should validate_uniqueness_of(:name) }
 
+   it "capitalizes the name" do
+    patient = Patient.new(:name => 'detroit')
+    patient.save
+    expect(patient.name).to eq "Detroit"
+  end
 
 end
